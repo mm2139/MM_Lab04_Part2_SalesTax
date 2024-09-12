077a9650de4d411ab5e0cc0067064644cc6e8863
mmaalin <maalin.622139@swl.k12.oh.us>
2024-09-12 08:24:27 -0400

    commit of lab 4 part 2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        // start code here
+        Scanner scan = new Scanner(System.in);
 
+        double itemPrice;
+        double itemPriceWithSalesTax;
+
+        System.out.println("Hello! Please input the price of your item to recieve a total with 5% sales tax applied.");
+        itemPrice = scan.nextDouble();
+        itemPriceWithSalesTax = itemPrice * 1.05;
+
+        System.out.println("The price of your " + itemPrice + " dollars purchase is with a 5% sales tax applied is " + itemPriceWithSalesTax + " dollars. The sales tax amounts to " + (itemPriceWithSalesTax-itemPrice) + " dollars.");
     }
 }
